9e6098a071b2cacb2dbb63b6c2e41542895861e2
Yaroslava <yarakucherenko07@gmail.com>
2024-09-15 22:42:09 +0300

    fix-task3-lesson6

--- lesson-6/task3.js
+++ lesson-6/task3.js
@@ -1,9 +1,11 @@
 function checkOrder (available, ordered) {
     if (available < ordered) {
         console.log("Your order is too large, we do not have enough goods.");
+        return;
     } else if (ordered === 0) {
         console.log("Your order is empty");
-    } else if (available > ordered) {
+        return;
+    } else {
         console.log("Your order is accepted");
     }
 }
