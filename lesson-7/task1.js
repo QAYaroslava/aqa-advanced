6553df8b6c4111b310d63e8fd4ffa0c6053b7340
Yaroslava <yarakucherenko07@gmail.com>
2024-09-18 09:10:53 +0300

    hometask-7

---
+++ lesson-7/task1.js
@@ -0,0 +1,18 @@
+function handleNum(number, handleEven, handleOdd) {
+    if (number % 2 === 0) {
+        console.log(handleEven())
+    } else {
+        console.log(handleOdd())
+    }
+}
+
+function handleEven() {
+    return "number is even";
+}
+
+function handleOdd() {
+    return "number is odd";
+}
+
+handleNum(10, handleEven, handleOdd);
+handleNum(11, handleEven, handleOdd);
